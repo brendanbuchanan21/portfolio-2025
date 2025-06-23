54d952dc97fedb689ac53de1afc131fbd91af5c2
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-06-23 10:54:38 -0500

    create hero-section base

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,16 @@ import { Link } from '@tanstack/react-router'
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 text-white justify-between bg-black border border-lime-400">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
-          <Link to='/projects'>Projects</Link>
-          <p>Contact</p>
-        </div>
-      </nav>
-    </header>
+    <header className="p-2 flex justify-start text-white bg-black h-[80px]">
+  <div className="flex justify-between items-center w-[30rem] px-4 py-2 ml-15">
+    <p className="text-2xl font-semibold text-[hsl(var(--primary-purple))]">Brendan.dev</p>
+    <nav className="flex gap-10 font-bold text-sm">
+      <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <p>Contact</p>
+    </nav>
+  </div>
+</header>
+
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,20 +7,46 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-   <div className="mesh-gradient-center flex justify-center items-center p-6 min-h-[calc(100vh-80px)] mx-auto overflow-hidden">
+   <div className="mesh-gradient-center flex flex-col items-start justify-start mx-auto overflow-hidden bg-square-pattern">
       {/* Your hero content here */}
-     <div>
+     <div className='flex flex-row gap-8 w-[50rem]'>
       <img src="/chatGptMe.png" alt="My image" className='w-30 h-30'/>
-      <h1 className="text-white text-5xl font-bold leading-relaxed">
-      Hey, I'm <span className="mr-2 text-[hsl(var(--primary-purple))]">Brendan</span>
+      <h1 className="text-white text-6xl font-bold leading-[1.1] font-[inter]">
+      Hey, I'm <span className="mr-2 text-[hsl(var(--primary-purple))] font-[Inter]">Brendan</span>
       <span className="inline-block mr-2">✨</span>
       <br />
-      <span className="mr-1">A</span>
-      <span className='text-[hsl(var(--primary-purple))]'>Software Engineer</span>
+      <span className="mr-1 font-[Inter]">A </span>
+      <span className='text-[hsl(var(--primary-purple))] font-[Inter]' >Software Engineer</span>
     </h1>
      </div>
 
+    {/* the main text of hero */}
+    <div className='w-[50rem] mt-7'>
+      <p className='text-xl'>A <span className='font-bold'>fullstack developer </span>with solid foundations in computer science principles.
+        Looking to the shape the future through cutting edge technology.
+      </p>
+    </div>
 
+    {/* links in hero  */}
+    <div className='mt-4  w-[50rem] gap-7 flex flex-row items-center'>
+      <button className='w-[9rem] h-[2.5rem] border border-gray-700 bg-black rounded-sm'>Contact Me</button>
+      <button className='w-[9rem] h-[2.5rem] border border-gray-700 bg-black rounded-sm'>Projects</button>
+
+      {/* Vertical divider */}
+    <div className="w-px h-[1.8rem] bg-gray-400 mx-3 flex justify-center items-center" />
+
+    <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+      <svg
+        className="w-5 h-5 fill-white hover:fill-gray-400 transition"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 .297a12 12 0 0 0-3.79 23.4c.6.113.82-.263.82-.582v-2.257c-3.338.726-4.042-1.612-4.042-1.612-.546-1.388-1.334-1.758-1.334-1.758-1.09-.745.083-.729.083-.729 1.204.085 1.837 1.236 1.837 1.236 1.07 1.834 2.807 1.304 3.492.997.108-.775.42-1.305.763-1.604-2.665-.305-5.467-1.334-5.467-5.933 0-1.31.467-2.381 1.236-3.22-.124-.304-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.51 11.51 0 0 1 6 0c2.29-1.552 3.297-1.23 3.297-1.23.655 1.653.243 2.872.12 3.176.77.839 1.235 1.91 1.235 3.22 0 4.61-2.807 5.625-5.48 5.922.43.372.814 1.103.814 2.222v3.293c0 .321.218.699.825.58A12 12 0 0 0 12 .297Z"/>
+      </svg>
+    </a>
+    </div>
+
+    
     </div>
   )
 }
